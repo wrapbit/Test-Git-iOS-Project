5f27d30114603d448c2ec8d21487da604a3fe796
wrapbit <gob@Wittayas-MacBook-Pro.local>
2014-09-18 14:41:46 +0700

    added a test class

---
+++ Git Test/GOBTest.h
@@ -0,0 +1,13 @@
+//
+//  GOBTest.h
+//  Git Test
+//
+//  Created by Wittaya Piyawannarat on 9/18/2557 BE.
+//  Copyright (c) 2557 Wittaya Piyawannarat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GOBTest : NSObject
+
+@end
---
+++ Git Test/GOBTest.m
@@ -0,0 +1,13 @@
+//
+//  GOBTest.m
+//  Git Test
+//
+//  Created by Wittaya Piyawannarat on 9/18/2557 BE.
+//  Copyright (c) 2557 Wittaya Piyawannarat. All rights reserved.
+//
+
+#import "GOBTest.h"
+
+@implementation GOBTest
+
+@end
